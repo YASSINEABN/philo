2a93d531571a841adb688f1986b7c3f35e378f9d
yaboulan <anoire.yassine19@gmail.com>
2024-05-08 20:55:15 +0100

    test

--- main.c
+++ main.c
@@ -5,30 +5,49 @@
 
 int i = 0;
 
+
+size_t get_time()
+{
+     struct timeval	time;
+     return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
 void assign_fork()
 {
     
 }
+
 void mutex_handle(pthread_mutex_t *mtx)
 {
     pthread_mutex_init(mtx,NULL);
 }
 
-void *application()
+void *application(void *philo)
 {
-    int a = ++i;
-    printf("%d",a);
+     t_philo *philoo = philo;
+     pthread_mutex_lock(&philoo->mtx);
+    printf("%d \n",++i);
+     pthread_mutex_unlock(&philoo->mtx);
 }
 
 void dinner_start(t_table *table)
 {
     int i = -1;
-    
+    table->time_start = get_time();
+
     while(++i <table->nbr_philos)
     {
-        pthread_create(&(table->philos[i].thread_id),NULL,&application,NULL);
-        usleep(100);
+        table->philos[i].start_time = get_time();
+        pthread_create(&(table->philos[i].thread_id),NULL,&application,&table->philos[i]);
+         usleep(100);
     }
+
+    i = -1;
+    while (++i < table->nbr_philos)
+    {
+        pthread_join(table->philos[i].thread_id, NULL);
+    }
+    
 }
 
 void init_struct(int argc , char **argv, t_table *table)
@@ -64,6 +83,8 @@ int main(int argc , char **argv)
         return -1;
     else 
     {
+        // struct timeval	time;
+        // printf("%ld \n",time.tv_sec * 1000 + time.tv_usec / 1000);
         init_struct(argc , argv+1 , &table);
         dinner_start(&table);
     }
--- philo.h
+++ philo.h
@@ -1,4 +1,5 @@
 #include<pthread.h>
+#include <sys/time.h>
 
 typedef struct s_table t_table;
 typedef struct philo t_philo;
@@ -6,10 +7,12 @@ typedef struct philo t_philo;
 typedef struct philo
 {
     int id;
-    long start_time;
+    size_t start_time;
     pthread_t thread_id;
     int meals_count;
     int full;
+    long last_meal;
+    char *state;
     t_table *table;
     pthread_mutex_t mtx;
     
@@ -20,6 +23,7 @@ typedef struct  s_table
     long time_to_die;
     long time_to_sleep;
     long time_to_eat;
+    size_t time_start;
     int nbr_philos;
     int nbr_limit_eat;
     t_philo *philos;
